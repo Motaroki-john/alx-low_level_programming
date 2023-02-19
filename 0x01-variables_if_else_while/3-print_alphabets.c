62ea879c49bd110d46d244d5074bf1926ddf4f26
Motaroki-john <johnmasire@yahoo.com>
2023-02-19 09:41:42 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+/**
+ *
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char c;
+
+
+	char d;
+
+
+	c= 'a';
+	c= 'A';
+	while
+                (c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z'){
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return 0;
+}
+
+		
+
+
+
+
