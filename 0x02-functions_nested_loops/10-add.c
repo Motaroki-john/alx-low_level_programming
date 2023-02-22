1cd7240c5adf0e8a5855326e968d653664d78561
Motaroki-john <johnmasire@yahoo.com>
2023-02-22 01:14:24 -0800

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,7 @@
+#include <main.h>
+
+
+int add(int 1, int 2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alpahabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int a);
+int _abs(int);
+int print_last_digit(int);
+int jack_bauer(void);
+int times_table(void);
+int add(int, int);
+void print_to_90(int n);
+void print_time_table(int a);
+
+#endif
+
+
